80596d9ef3049c4bdfbe0c2050e5f15e1189a719
mariia-gg <superkomarnitska@gmail.com>
2022-11-01 19:54:00 +0200

    Homework 4_1 and 4_2

---
+++ homework4_1/homework4_1/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace homework4_1_methods_
+{
+    /////  HOMEWORK4_1
+    internal class HOMEWORK4_1
+    {
+        public static int GetMaxValue(int FirstParameter, int SecondParameter)
+        {
+            if (FirstParameter > SecondParameter)
+            {
+                Console.WriteLine($"The First Parameter greater = {FirstParameter}");
+                return FirstParameter;
+            }
+            if (SecondParameter > FirstParameter)
+            {
+                Console.WriteLine($"The Second Parameter greater = {SecondParameter}");
+                return SecondParameter;
+            }
+            else
+            {
+                throw new Exception("The parameters are identical");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value for first parameter:\t");
+            int FirstParameter = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter value for second parameter:\t");
+            int SecondParameter = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMaxValue(FirstParameter, SecondParameter));
+        }
+    }
+        ///////// HOMEWORK4_2////////// 
+        internal class Homework4_2
+        {
+            public static int GetMaxValue(int FirstParameter, int SecondParameter)
+            {
+                if (FirstParameter < SecondParameter)
+                {
+                    Console.WriteLine($"The First Parameter lower than Second Parameter  = {SecondParameter}");
+                    return FirstParameter;
+                }
+                if (SecondParameter < FirstParameter)
+                {
+                    Console.WriteLine($"The Second Parameter lower than First Parameter = {FirstParameter}");
+                    return SecondParameter;
+                }
+                else
+                {
+                    throw new Exception("The parameters are identical");
+                }
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter value for first parameter:\t");
+                int FirstParameter = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter value for second parameter:\t");
+                int SecondParameter = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(GetMaxValue(FirstParameter, SecondParameter));
+
+                Console.ReadKey(); 
+            }
+        }
+
+
+    
+
+}
