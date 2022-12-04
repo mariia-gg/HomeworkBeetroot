ed413ace71bc0d7e0565f2e005d249140cc08840
mariia-gg <superkomarnitska@gmail.com>
2022-12-04 22:08:51 +0200

    book library without validation

---
+++ BookLibraryHM10/LibraryDataAccess/Repository.cs
@@ -0,0 +1,66 @@
+﻿using LibraryEntities;
+using Newtonsoft.Json;
+
+namespace LibraryDataAccess
+{
+    public class Repository<TEntity> : LibraryEntity, IRepository<TEntity> where TEntity : IEntity
+        {
+            private static readonly string DbFilePathTemplate = @"C:\LibraryDbFolder\lib.json_{0}";
+            private static readonly string DbFilePath = string.Format(DbFilePathTemplate, typeof(TEntity));
+            private List<TEntity> _storage = ReadStorage();
+
+
+            public IList<TEntity> GetAll()
+            {
+                return _storage;
+            }
+
+            public TEntity Get(int id)
+            {
+                var storageEntity = _storage.First(ent => ent.Id == id);
+
+                return storageEntity;
+            }
+
+            public void Insert(TEntity entity)
+            {
+                if (entity.Id <= 0)
+                    entity.Id = (_storage.OrderByDescending(ent => ent.Id).FirstOrDefault()?.Id ?? 0) + 1;
+
+                _storage.Add(entity);
+                SaveStorage(_storage);
+            }
+
+            private static List<TEntity> ReadStorage()
+            {
+                EnsureDbFile();
+
+                var text = File.ReadAllText(DbFilePath);
+
+                var entities = JsonConvert.DeserializeObject<List<TEntity>>(text);
+
+                return entities ?? new List<TEntity>();
+            }
+
+            private static void SaveStorage(List<TEntity> entities)
+            {
+                var text = JsonConvert.SerializeObject(entities);
+                File.WriteAllText(DbFilePath, text);
+            }
+
+            private static void EnsureDbFile()
+            {
+                if (!File.Exists(DbFilePath))
+                {
+                    using (File.Create(DbFilePath))
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
---
+++ BookLibraryHM10/Program.cs
@@ -0,0 +1,17 @@
+﻿using LibraryDataAccess;
+using LibraryServices;
+
+namespace LibraryEntities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new Service();
+            service.Initialize();
+            service.EntryInLibrary();
+
+            Console.ReadKey();
+        }
+    }
+}
