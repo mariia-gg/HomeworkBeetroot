4fe5dbe960ed44f8b11dcffa20eceacfe57921e2
mariia-gg <superkomarnitska@gmail.com>
2022-12-08 22:36:47 +0200

    Finally added all methods

--- Mystack/Mystack/Program.cs
+++ Mystack/Mystack/Program.cs
@@ -7,29 +7,78 @@ namespace Mystack
     {
     static void Main(string[] args)
     {
-        CreatingStack(); 
+          CreatingStack();
+          Console.WriteLine();
+          ArrayCreating(); 
     }
 
-        public static void CreatingStack()
+    public static void CreatingStack()
+    {
+         Stack<int> myStack = new Stack<int>();
+
+         myStack.Push(1);   
+         myStack.Push(2);
+         myStack.Push(3);  
+         myStack.Push(4);  
+         myStack.Push(5);      
+
+         Console.WriteLine("The initial stack: ");    
+         foreach (var number in myStack)
+         {
+             Console.Write(number + ",");
+         }   
+
+         Console.WriteLine();
+            
+         Console.WriteLine("Peek in the stack:{0} ", myStack.Peek());
+
+         Console.WriteLine("Pop in the stack:{0} ", myStack.Pop());
+
+         foreach(var number in myStack)
+         {
+             Console.Write(number + ",");
+         }
+
+         Console.WriteLine();
+         Console.WriteLine("Number of elements: "); 
+         Console.WriteLine(myStack.Count);
+
+         myStack.Clear();
+        }
+
+        public static void ArrayCreating()
         {
-            Stack<int> myStack = new Stack<int>();
+            Stack myStack1 = new Stack();
 
-            myStack.Push(1);
-            myStack.Push(2);
-            myStack.Push(3);
-            myStack.Push(4);
-            myStack.Push(5);
+            myStack1.Push("Lazy cat, ");
 
-            foreach (var number in myStack)
-            {
-                Console.Write(number + ",");
-            }
+            Array mySuperPuperArray = Array.CreateInstance(typeof(string), 15);
+
+            mySuperPuperArray.SetValue("", 0);
+            mySuperPuperArray.SetValue("have", 1);
+            mySuperPuperArray.SetValue("a", 2);
+            mySuperPuperArray.SetValue("nice", 3);
+            mySuperPuperArray.SetValue("day", 4);
+
+            Console.WriteLine("My super puper Array:");
+
+            PrintValues(mySuperPuperArray, ' ');
 
             Console.WriteLine();
-            
-            Console.WriteLine("Peek in the stack:{0} ", myStack.Peek());
+            Console.WriteLine("Adding  CopyToArray");
 
+            myStack1.CopyTo(mySuperPuperArray, 0);
 
+            PrintValues(mySuperPuperArray, ' ');
+        }
+
+        public static void PrintValues(Array myArr, char mySeparator)
+        {
+            foreach (Object myObj in myArr)
+            {
+                Console.Write("{0}{1}", mySeparator, myObj);
+            }
+            Console.WriteLine();
         }
 
     }
