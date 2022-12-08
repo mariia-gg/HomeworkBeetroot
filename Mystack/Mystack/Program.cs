5af1c729fcc67fdaed7b401ef39c82349f9b6f5b
mariia-gg <superkomarnitska@gmail.com>
2022-12-08 18:54:01 +0200

    Push added

--- Mystack/Mystack/Program.cs
+++ Mystack/Mystack/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Mystack
+﻿using System;
+using System.Collections;
+
+namespace Mystack
 {
     internal class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        CreatingStack(); 
+    }
+
+    public static void CreatingStack()
+    {       
+        Stack<int> myStack = new Stack<int>();
+        
+        myStack.Push(1);
+        myStack.Push(2);
+        myStack.Push(3);
+        myStack.Push(4);
+        myStack.Push(5);
+     
+        foreach(var number in myStack)
         {
-            Console.WriteLine("Hello, World!");
+             Console.Write(number + ",");
         }
+        }
+
     }
 }
