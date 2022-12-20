600fc0acaf2a759a6ce4f1f8e51876a9e5972359
mariia-gg <superkomarnitska@gmail.com>
2022-12-20 17:41:12 +0200

    file from lesson

---
+++ LINQ Lesson/LINQ Lesson/Program.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace LinqLesson;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+    }
+}
