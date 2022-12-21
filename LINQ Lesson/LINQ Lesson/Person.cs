2ebfae62e81e4662897be40f18d097846a9f2886
mariia-gg <superkomarnitska@gmail.com>
2022-12-21 23:18:03 +0200

    created some methods

--- LINQ Lesson/LINQ Lesson/Person.cs
+++ LINQ Lesson/LINQ Lesson/Person.cs
@@ -10,12 +10,12 @@ public class Friend
 {
     public int Id { get; set; }
 
-    public string? Name { get; set; }
+    public string Name { get; set; }
 }
 
-public class Person
+public class Person 
 {
-    public string? Id { get; set; }
+    public string Id { get; set; }
 
     public int Index { get; set; }
 
@@ -23,33 +23,35 @@ public class Person
 
     public bool IsActive { get; set; }
 
-    public string? Balance { get; set; }
+    public string Balance { get; set; }
 
     public int Age { get; set; }
 
-    public string? EyeColor { get; set; }
+    public string EyeColor { get; set; }
 
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
     public Gender Gender { get; set; }
 
-    public string? Company { get; set; }
+    public string Company { get; set; }
 
-    public string? Email { get; set; }
+    public string Email { get; set; }
 
-    public string? Phone { get; set; }
+    public string Phone { get; set; }
 
-    public string? Address { get; set; }
+    public string Address { get; set; }
 
-    public string? About { get; set; }
+    public string About { get; set; }
 
     public DateTime Registered { get; set; }
 
-    public double Latitude { get; set; }
+    public string[] Tags { get; set; }
+
+    public Friend[] Friends { get; set; }
 
     public double Longitude { get; set; }
 
-    public string[]? Tags { get; set; }
+    public double Latitude { get; set; }
 
-    public Friend[]? Friends { get; set; }
 }
+
--- LINQ Lesson/LINQ Lesson/Program.cs
+++ LINQ Lesson/LINQ Lesson/Program.cs
@@ -1,11 +1,43 @@
 ﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using LinqLesson;
 
 namespace LinqLesson;
 
-internal class Program
+internal class Program 
 {
     private static void Main(string[] args)
     {
-        var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+        var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json")).ToList();
+
+        var male = persons.Count(p => p.Gender == Gender.Male);
+        var female = persons.Count(p => p.Gender == Gender.Female);
+
+        var maxFriends = persons.OrderByDescending(p => p.Friends.Length).First();
+
+        //find out who is located farthest north / south / west / east using latitude/ longitude data
+      
+
+
+        //find max and min distance between 2 persons
+        var orderedList = persons.GroupBy(x=> x.Latitude).Max();
+        //find 2 persons whos ‘about’ have the most same words
+
+        var about  = persons.GroupBy(p=>p.About).Where(g => g.Count() > 1).Select(y => y.Key).ToList();
+
+        //find persons with same friends(compare by friend’s name)
+
+        var theSameFriends = persons.GroupBy(p => p.Friends); 
+
+
+
+
+
+        //var userList1 = new List<string>(); 
+        //var friends= persons.Where(p=> p.Friends)
+        //persons.ForEach();
+
     }
 }
